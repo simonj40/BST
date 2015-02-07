2c77e8839430324f0bd7179afc23f3ddee3093ce
simon <simonj40@gmail.com>
2015-02-07 23:55:40 +0100

    Implements multi thread safe & Benchmark
    - Implemented MultiThreads with callable calculating and returning its
    execution duration
    - Made thread safe the BST by using lock on the added method and the
    toDot method
    - Added time calculation for adding a node. The adding time is stored
    in each node object of the tree.
    - Benchmark, retrieve and display average adding time,  and average
    thread execution time

--- src/org/fauberteau/rbtree/BinarySearchTree.java
+++ src/org/fauberteau/rbtree/BinarySearchTree.java
@@ -27,6 +27,8 @@
 package org.fauberteau.rbtree;
 
 import java.util.ArrayDeque;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Implement a Binary Search Tree (BST) data structure. It is not thread-safe
@@ -35,141 +37,173 @@ import java.util.ArrayDeque;
  *
  */
 public class BinarySearchTree<E extends Comparable<E>> {
-  
-  private BinarySearchNode<E> root;
-  
-  /**
-   * Add an element in the tree if it is not already present.
-   * 
-   * @param e
-   *          the element to add in the tree
-   * @return true if the element has been correctly added and false if it is
-   *         already present
-   */
-  public synchronized boolean add(E e) {
-    BinarySearchNode<E> y = null;
-    BinarySearchNode<E> x = getRoot();
-    while (x != null) {
-      y = x;
-      if (x.getKey().compareTo(e) == 0) {
-        return false;
-      } else if (x.getKey().compareTo(e) > 0) {
-        x = x.getRightChild();
-      } else {
-        x = x.getLeftChild();
-      }
-    }
-    
-    BinarySearchNode<E> z = new BinarySearchNode<>(e);
-    
-    if (y == null) {
-      setRoot(z);
-    } else {
-      if (y.getKey().compareTo(e) == 0) {
-        return false;
-      } else if (y.getKey().compareTo(e) > 0) {
-        y.setRightChild(z);
-      } else {
-        y.setLeftChild(z);
-      }
-    }
-    return true;
-  }
-  
-  public void randomlyAdd(int count){
-	  
-	  for(int i=0; i<count;i++){
-		  
-		  
-		  
-		  
-		  
-		  
-	  }
-	  
-	  
-	  
-  }
-  
-  
-  private BinarySearchNode<E> getRoot() {
-    return root;
-  }
-  
-  private void setRoot(BinarySearchNode<E> node) {
-    root = node;
-  }
-  
-  
-  public String toDOT(String name) {
-    ArrayDeque<BinarySearchNode<E>> queue = new ArrayDeque<>();
-    StringBuilder sb = new StringBuilder();
-    if (getRoot() != null) {
-      sb.append("graph ").append(name).append(" {\n");
-      queue.offerFirst(getRoot());
-      while (!queue.isEmpty()) {
-        BinarySearchNode<E> node = queue.pollLast();
-        if (node.hasRightChild()) {
-          BinarySearchNode<E> right = node.getRightChild();
-          sb.append(node).append(" -- ").append(right).append("\n");
-          queue.offerFirst(right);
-        }
-        if (node.hasLeftChild()) {
-          BinarySearchNode<E> left = node.getLeftChild();
-          sb.append(node).append(" -- ").append(left).append(";\n");
-          queue.offerFirst(left);
-        }
-      }
-      sb.append('}');
-    }
-    return sb.toString();
-  }
-  
-  private static class BinarySearchNode<E extends Comparable<E>> {
-    
-    private final E key;
- 
-    private BinarySearchNode<E> left;
-    
-    private BinarySearchNode<E> right;
-    
-    private BinarySearchNode(E key) {
-      this.key = key;
-    }
-    
-    private E getKey() {
-      return key;
-    }
-    
-    private BinarySearchNode<E> getLeftChild() {
-      return left;
-    }
-    
-    private BinarySearchNode<E> getRightChild() {
-      return right;
-    }
-    
-    private boolean hasLeftChild() {
-      return getLeftChild() == null ? false : true;
-    }
-    
-    private boolean hasRightChild() {
-      return getRightChild() == null ? false : true;
-    }
-   
-    private void setLeftChild(BinarySearchNode<E> node) {
-      left = node;
-    }
-    
-    private void setRightChild(BinarySearchNode<E> node) {
-      right = node;
-    }
-    
-    @Override
-    public String toString() {
-      return getKey().toString();
-    }
-    
-  }
+
+	private BinarySearchNode<E> root;
+
+	private Lock lock = new ReentrantLock();
+
+	/**
+	 * Add an element in the tree if it is not already present.
+	 * 
+	 * @param e
+	 *            the element to add in the tree
+	 * @return true if the element has been correctly added and false if it is
+	 *         already present
+	 */
+	public boolean add(E e) {
+		long start = System.currentTimeMillis();
+		lock.lock();
+		BinarySearchNode<E> y = null;
+		BinarySearchNode<E> x = getRoot();
+		while (x != null) {
+			y = x;
+			if (x.getKey().compareTo(e) == 0) {
+				lock.unlock();
+				return false;
+			} else if (x.getKey().compareTo(e) > 0) {
+				x = x.getRightChild();
+			} else {
+				x = x.getLeftChild();
+			}
+		}
+
+		BinarySearchNode<E> z = new BinarySearchNode<>(e);
+
+		if (y == null) {
+			setRoot(z);
+		} else {
+			if (y.getKey().compareTo(e) == 0) {
+				System.out.println("LOCK !");
+				lock.unlock();
+				return false;
+			} else if (y.getKey().compareTo(e) > 0) {
+				y.setRightChild(z);
+			} else {
+				y.setLeftChild(z);
+			}
+		}
+		z.setTime(System.currentTimeMillis() - start);
+		lock.unlock();
+		return true;
+	}
+
+	private BinarySearchNode<E> getRoot() {
+		return root;
+	}
+
+	private void setRoot(BinarySearchNode<E> node) {
+		root = node;
+	}
+
+	public String toDOT(String name) {
+		lock.lock();
+		ArrayDeque<BinarySearchNode<E>> queue = new ArrayDeque<>();
+		StringBuilder sb = new StringBuilder();
+		if (getRoot() != null) {
+			sb.append("graph ").append(name).append(" {\n");
+			queue.offerFirst(getRoot());
+			while (!queue.isEmpty()) {
+				BinarySearchNode<E> node = queue.pollLast();
+				if (node.hasRightChild()) {
+					BinarySearchNode<E> right = node.getRightChild();
+					sb.append(node).append(" -- ").append(right).append("\n");
+					queue.offerFirst(right);
+				}
+				if (node.hasLeftChild()) {
+					BinarySearchNode<E> left = node.getLeftChild();
+					sb.append(node).append(" -- ").append(left).append(";\n");
+					queue.offerFirst(left);
+				}
+			}
+			sb.append('}');
+		}
+		lock.unlock();
+		return sb.toString();
+	}
+
+	public double getTime() {
+
+		double time = 0;
+		int count = 0;
+		ArrayDeque<BinarySearchNode<E>> queue = new ArrayDeque<>();
+
+		if (getRoot() != null) {
+			queue.offerFirst(getRoot());
+			while (!queue.isEmpty()) {
+				BinarySearchNode<E> node = queue.pollLast();
+				time += node.getTime();
+				count++;
+				if (node.hasRightChild()) {
+					BinarySearchNode<E> right = node.getRightChild();
+					queue.offerFirst(right);
+				}
+				if (node.hasLeftChild()) {
+					BinarySearchNode<E> left = node.getLeftChild();
+					queue.offerFirst(left);
+				}
+			}
+			
+			time = time/count;
+		}
+
+		return time;
+	}
+
+	private static class BinarySearchNode<E extends Comparable<E>> {
+
+		private final E key;
+
+		private BinarySearchNode<E> left;
+
+		private BinarySearchNode<E> right;
+
+		private long time;
+
+		private BinarySearchNode(E key) {
+			this.key = key;
+		}
+
+		private E getKey() {
+			return key;
+		}
+
+		private BinarySearchNode<E> getLeftChild() {
+			return left;
+		}
+
+		private BinarySearchNode<E> getRightChild() {
+			return right;
+		}
+
+		private boolean hasLeftChild() {
+			return getLeftChild() == null ? false : true;
+		}
+
+		private boolean hasRightChild() {
+			return getRightChild() == null ? false : true;
+		}
+
+		private void setLeftChild(BinarySearchNode<E> node) {
+			left = node;
+		}
+
+		private void setRightChild(BinarySearchNode<E> node) {
+			right = node;
+		}
+
+		private void setTime(long time) {
+			this.time = time;
+		}
+		
+		private long getTime(){
+			return time;
+		}
+
+		@Override
+		public String toString() {
+			return getKey().toString();
+		}
+
+	}
 
 }
--- src/org/fauberteau/rbtree/Main.java
+++ src/org/fauberteau/rbtree/Main.java
@@ -28,23 +28,102 @@ package org.fauberteau.rbtree;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
-  
-  public static final void main(String[] args) throws IOException {
-    String name = "rbtree";
-    BinarySearchTree<Integer> rbtree = new BinarySearchTree<>();
-    rbtree.add(10);
-    rbtree.add(15);
-    rbtree.add(8);
-    rbtree.add(12);
-    rbtree.add(4);
-    rbtree.add(9);
-    PrintWriter writer = new PrintWriter(name + ".dot");
-    writer.println(rbtree.toDOT(name));
-    writer.close();
-    ProcessBuilder builder = new ProcessBuilder("dot", "-Tpdf", "-o", name + ".pdf", name + ".dot");
-    builder.start();
-  }
-  
+
+	private final static int MAX_THREADS = 10000;
+	private  final static ExecutorService service = Executors
+			.newFixedThreadPool(MAX_THREADS);
+
+	private static int THREADS_NUM = 100;
+	private static int ADD_PER_THREADS = 1000;
+
+	public static BinarySearchTree<Integer> rbtree = new BinarySearchTree<>();
+
+	public static final void main(String[] args) throws IOException {
+		
+		double AVG_ADDING_TIME = 0;
+		double AVG_TIME_THREAD = 0;
+		ArrayList<Future<Long>> futureList = new ArrayList<>();
+		
+	
+		RandomGenerator generator = new RandomGenerator(ADD_PER_THREADS);
+		
+		for (int i = 0; i < THREADS_NUM; i++) {
+			Future<Long> future = service.submit(generator);
+			futureList.add(future);
+			
+		}
+		
+		
+		for (Future<Long> future : futureList) {
+			try {
+				AVG_TIME_THREAD += future.get().longValue();
+			} catch (InterruptedException | ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		service.shutdown();
+		
+		AVG_TIME_THREAD = AVG_TIME_THREAD/THREADS_NUM;
+		
+		
+		
+		AVG_TIME_THREAD = AVG_TIME_THREAD/THREADS_NUM;
+		
+		AVG_ADDING_TIME = rbtree.getTime();
+		
+		System.out.println();
+		System.out.println("NUMBER OF THREADS : " + THREADS_NUM);
+		System.out.println("ADD PER THREADS : " + ADD_PER_THREADS);
+		System.out.println("AVERAGE ADDING TIME : " + AVG_ADDING_TIME);
+		System.out.println("AVERAGE TIME PER THREADS : " + AVG_TIME_THREAD);
+		
+
+		String name = "rbtree";
+		PrintWriter writer = new PrintWriter(name + ".dot");
+		writer.println(rbtree.toDOT(name));
+		writer.close();
+		ProcessBuilder builder = new ProcessBuilder("/usr/local/bin/dot", "-Tpdf", "-o", name + ".pdf ", name + ".dot");
+		builder.start();
+		
+		
+		
+	}
+
+	public static class RandomGenerator implements Callable<Long> {
+
+		private int count;
+
+		public RandomGenerator(int count) {
+			this.count = count;
+		}
+
+		
+		/* (non-Javadoc)
+		 * @see java.util.concurrent.Callable#call()
+		 */
+		@Override
+		public Long call(){
+			long start = System.currentTimeMillis();
+			
+			Random rand = new Random();
+
+			for (int i = 0; i < count; i++) {
+				rbtree.add(rand.nextInt());
+			}
+			
+			return System.currentTimeMillis() - start;
+		}
+
+	}
+
 }
